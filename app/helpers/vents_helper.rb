fadfb9ec8fb16efa2a93c17d8c80181ff3d19d32
Matt Rice <mattrice12@outlook.com>
2017-02-17 23:59:15 -0600

    Added larger i18n library for es and fr; finished translations for the rest of the site.

--- app/helpers/vents_helper.rb
+++ app/helpers/vents_helper.rb
@@ -1,5 +1,5 @@
 module VentsHelper
   def submit_time(vent)
-    t("datetime.distance_in_words.submitted", count: distance_of_time_in_words(Time.now, vent.created_at))
+    t("time.submitted", count: distance_of_time_in_words(Time.now, vent.created_at))
   end
 end
